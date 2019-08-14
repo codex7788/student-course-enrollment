b4a544f9ddfdce03384cc9086370af1906f4ba00
nbn <nabin62246@gmail.com>
2019-08-14 13:10:22 +0545

    modified lazy loading by explicit loading

--- ContosoUniversity/Controllers/InstructorController.cs
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -20,6 +20,7 @@ namespace ContosoUniversity.Controllers
         public ActionResult Index(int? id,int? courseID)
         {
             var viewModel = new InstructorIndexData();
+
             viewModel.Instructors = db.Instructors
                 .Include(i => i.OfficeAssignment)
                 .Include(i => i.Courses.Select(c => c.Department))
@@ -33,8 +34,13 @@ namespace ContosoUniversity.Controllers
             if(courseID!=null)
             {
                 ViewBag.CourseID = courseID.Value;
-                viewModel.Enrollments = viewModel.Courses.Where(
-                    x => x.CourseID == courseID).Single().Enrollments;
+                var selectedCourse = viewModel.Courses.Where(x => x.CourseID == courseID).Single();
+                db.Entry(selectedCourse).Collection(x => x.Enrollments).Load();
+                foreach(Enrollment enrollment in selectedCourse.Enrollments)
+                {
+                    db.Entry(enrollment).Reference(x => x.Student).Load();
+                }
+                viewModel.Enrollments = selectedCourse.Enrollments;
             }
             return View(viewModel);
         }
